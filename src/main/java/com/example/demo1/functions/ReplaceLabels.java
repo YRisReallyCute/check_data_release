3e3046981c177fc78d2dcfa1d22fccaa83a85a28
SJQ <1757268018@qq.com>
2019-09-01 20:16:08 +0800

    完善 > <

--- src/main/java/com/example/demo1/functions/ReplaceLabels.java
+++ src/main/java/com/example/demo1/functions/ReplaceLabels.java
@@ -9,11 +9,11 @@ import java.util.Optional;
 @Component
 public class ReplaceLabels {
 
-    private static String str1_0="<span class=\"title_1\">";
+    private static String str1_0="<span class='title_1'>";
 
     private static String str1_1="</span>";
 
-    private static String str2_0="<span class=\"title_2\">";
+    private static String str2_0="<span class='title_2'>";
 
     private static String str2_1="</span>";
 
@@ -31,6 +31,23 @@ public class ReplaceLabels {
 
     public String getStr2_1() {
         return str2_1;
+    }
+
+
+    public String rep(ReplaceLabels replaceLabels, String strToReplace){
+        String str1_0 = replaceLabels.getStr1_0();
+        String str1_1 = replaceLabels.getStr1_1();
+        String str2_0 = replaceLabels.getStr2_0();
+        String str2_1 = replaceLabels.getStr2_1();
+
+        return strToReplace
+                .replaceAll("<(?!(h1>)|(/h1>)|(p>)|(br/>)|(br />))","小于") // 非标签的 < 更换为 小于
+                .replaceAll("(?<!(<h1)|(</h1)|(<p)|(<br/)|(<br /))>","大于")  // 非标签的 > 更换为 大于
+                .replaceAll("<h1>",str1_0)
+                .replaceAll("</h1><br/>",str1_1)
+                .replaceAll("<h2>",str2_0)
+                .replaceAll("</h2><br/>",str2_1)
+                .replaceAll("<!--.*-->", "");
     }
 
 }
